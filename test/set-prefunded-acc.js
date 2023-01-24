bb6ff720fa33e7544dd45b286ace77c20799626b
Dmitry Osipov <dmith.osipov@gmail.com>
2023-01-24 16:08:30 +0300

    take prefunded accounts from config.json

--- test/set-prefunded-acc.js
+++ test/set-prefunded-acc.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
 const assert = require('assert');
 
-const path = process.argv[2];
+const [path, configPath] = process.argv.slice(2);
 assert.notStrictEqual(path, undefined, 'Path to gear spec file is not specified');
+assert.notStrictEqual(configPath, undefined, 'Path to rosetta config file is not specified');
 
-
-const balance = `,
-          [
-            "5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC", 
+let spec = fs.readFileSync(path, 'utf-8');
+let config = fs.readFileSync(configPath, 'utf-8');
+const accounts = JSON.parse(config).construction.prefunded_accounts.map(({ account_identifier: { address }}) => 
+`          [
+            "${address}", 
             1152921504606846976
           ]
-        ]
-      }`
+`).join(',\n');
 
-let spec = fs.readFileSync(path, 'utf-8')
+const balance = `,
+${accounts}
+        ]
+      }`;
+
 const match = spec.match(/"balances": \[[\w\W]+?\}/g)[0]
 json = spec.replace(match, match.replace(/\s*\]\s*\}/g, balance))
 
