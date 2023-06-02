5e300f16042b385ac85786b4eee1c2949f62b5d6
Dmitry Osipov <dmith.osipov@gmail.com>
2023-06-02 14:53:21 +0400

    send 500 status if error

--- server/src/helpers/errors.ts
+++ server/src/helpers/errors.ts
@@ -1,6 +1,7 @@
 import { Error } from 'rosetta-client';
 
 interface E {
+  code: number;
   message: string;
   description?: string;
   retriable: boolean;
@@ -8,38 +9,43 @@ interface E {
 }
 
 export enum ApiError {
-  NOT_SUPPORTED,
-  NOT_IMPLEMENTED,
-  SIG_TYPE_NOT_SUPPORTED,
-  NETWORK_NOT_SUPPORTED,
-  TRANSACTION_NOT_FOUND,
-  BROADCAST_TRANSACTION,
-  INVALID_OPERATIONS_LENGTH,
-  TRANSACTION_IS_OUTDATED,
-  TRANSACTION_BAD_SIG,
-  NOT_AVAILABLE_OFFLINE,
-  UNABLE_TO_GET_BLOCK,
-  INVALID_ACCOUNT_ADDRESS,
+  NOT_SUPPORTED = 600,
+  NOT_IMPLEMENTED = 601,
+  SIG_TYPE_NOT_SUPPORTED = 602,
+  NETWORK_NOT_SUPPORTED = 603,
+  TRANSACTION_NOT_FOUND = 604,
+  BROADCAST_TRANSACTION = 605,
+  INVALID_OPERATIONS_LENGTH = 606,
+  TRANSACTION_IS_OUTDATED = 607,
+  TRANSACTION_BAD_SIG = 608,
+  NOT_AVAILABLE_OFFLINE = 609,
+  UNABLE_TO_GET_BLOCK = 610,
+  INVALID_ACCOUNT_ADDRESS = 611,
 }
 
 const errors: Record<number, E> = {
   [ApiError.NOT_SUPPORTED]: {
+    code: ApiError.NOT_SUPPORTED,
     message: 'Endpoint is not supported',
     retriable: false,
   },
   [ApiError.NOT_IMPLEMENTED]: {
+    code: ApiError.NOT_IMPLEMENTED,
     message: 'Endpoint is not implemented',
     retriable: false,
   },
   [ApiError.SIG_TYPE_NOT_SUPPORTED]: {
+    code: ApiError.SIG_TYPE_NOT_SUPPORTED,
     message: 'Signature type is not supported',
     retriable: false,
   },
   [ApiError.NETWORK_NOT_SUPPORTED]: {
+    code: ApiError.NETWORK_NOT_SUPPORTED,
     message: 'Network is not supported',
     retriable: false,
   },
   [ApiError.TRANSACTION_NOT_FOUND]: {
+    code: ApiError.TRANSACTION_NOT_FOUND,
     message: 'Transaction not found',
     retriable: false,
     details: {
@@ -47,26 +53,32 @@ const errors: Record<number, E> = {
     },
   },
   [ApiError.BROADCAST_TRANSACTION]: {
+    code: ApiError.BROADCAST_TRANSACTION,
     message: 'Broadcast transaction error',
     retriable: false,
   },
   [ApiError.INVALID_OPERATIONS_LENGTH]: {
+    code: ApiError.INVALID_OPERATIONS_LENGTH,
     message: 'Operations length is invalid',
     retriable: false,
   },
   [ApiError.TRANSACTION_IS_OUTDATED]: {
+    code: ApiError.TRANSACTION_IS_OUTDATED,
     message: 'Transaction is outdated',
     retriable: false,
   },
   [ApiError.TRANSACTION_BAD_SIG]: {
+    code: ApiError.TRANSACTION_BAD_SIG,
     message: 'Transaction has a bad signature',
     retriable: false,
   },
   [ApiError.NOT_AVAILABLE_OFFLINE]: {
+    code: ApiError.NOT_AVAILABLE_OFFLINE,
     message: 'Endpoint is not available in offline mode',
     retriable: false,
   },
   [ApiError.UNABLE_TO_GET_BLOCK]: {
+    code: ApiError.UNABLE_TO_GET_BLOCK,
     message: 'Unable to retrieve block by specified hash or number',
     retriable: false,
     details: {
@@ -75,16 +87,17 @@ const errors: Record<number, E> = {
     },
   },
   [ApiError.INVALID_ACCOUNT_ADDRESS]: {
+    code: ApiError.INVALID_ACCOUNT_ADDRESS,
     message: 'Account address is invalid',
     retriable: false,
   },
 };
 
-const code = 500;
-
 export function throwError(errorCode: ApiError, details?: object) {
-  const { message, retriable } = errors[errorCode];
+  const { message, retriable, code } = errors[errorCode];
   throw Error.constructFromObject({ code, message, retriable, details });
 }
 
-export const allErrors = Object.values(errors).map((e) => new Error(code, e.message, e.retriable));
+export const allErrors = Object.values(errors).map(
+  ({ code, message, retriable }) => new Error(code, message, retriable),
+);
