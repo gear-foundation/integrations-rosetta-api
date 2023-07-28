a0eba61531fcbb3d87aa7a90d145a286f2a8babd
Stefan Mendoza <stefan.mendoza@coinbase.com>
2023-07-28 12:21:55 -0400

    testing

--- test/set-prefunded-acc.js
+++ test/set-prefunded-acc.js
@@ -9,7 +9,7 @@ let spec = fs.readFileSync(path, 'utf-8');
 let config = fs.readFileSync(configPath, 'utf-8');
 const accounts = JSON.parse(config).construction.prefunded_accounts.map(({ account_identifier: { address }}) => 
 `          [
-            "${address}", 
+            "e344fd5e18d8ef2a08bb61d1e66bd2a6c0c1a5509c6317f373793d148b0b2534",
             1152921504606846976
           ]
 `).join(',\n');
