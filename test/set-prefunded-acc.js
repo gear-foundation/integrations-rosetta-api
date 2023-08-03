0f7d497f972dda55b7b51d3f002c86708f3a4cab
Dmitry Osipov <dmith.osipov@gmail.com>
2023-08-03 15:07:44 +0400

    fix: set prefunded accounts script

--- test/set-prefunded-acc.js
+++ test/set-prefunded-acc.js
@@ -7,19 +7,23 @@ assert.notStrictEqual(configPath, undefined, 'Path to rosetta config file is not
 
 let spec = fs.readFileSync(path, 'utf-8');
 let config = fs.readFileSync(configPath, 'utf-8');
-const accounts = JSON.parse(config).construction.prefunded_accounts.map(({ account_identifier: { address }}) => 
-`          [
-            "${address}", 
+const accounts = JSON.parse(config)
+  .construction.prefunded_accounts.map(
+    ({ account_identifier: { address } }) =>
+      `          [
+            "5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC", 
             1152921504606846976
           ]
-`).join(',\n');
+`,
+  )
+  .join(',\n');
 
 const balance = `,
 ${accounts}
         ]
       }`;
 
-const match = spec.match(/"balances": \[[\w\W]+?\}/g)[0]
-json = spec.replace(match, match.replace(/\s*\]\s*\}/g, balance))
+const match = spec.match(/"balances": \[[\w\W]+?\}/g)[0];
+json = spec.replace(match, match.replace(/\s*\]\s*\}/g, balance));
 
-fs.writeFileSync(path, json)
+fs.writeFileSync(path, json);
