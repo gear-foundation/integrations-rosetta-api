ca021e43eb1b8fb13532cb0996a215f648577235
Dmitry Osipov <dmith.osipov@gmail.com>
2023-10-05 14:53:09 +0400

    fix: make transfers

--- test/make-transfers.js
+++ test/make-transfers.js
@@ -7,35 +7,29 @@ const main = async () => {
   const alice = keyring.addFromUri('//Alice', {}, 'sr25519');
   const bob = keyring.addFromUri('//Bob', {}, 'sr25519');
 
-  await api.tx.balances
-    .transferKeepAlive('5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC', 1000 * 10 ** 12)
-    .signAndSend(alice, ({ events }) => {
-      const transferEvent = events.find(
-        ({ event: { method, section } }) => section === 'system' && method === 'Transfer',
-      );
+  const tx = api.tx.utility.batchAll([
+    api.tx.balances.transferKeepAlive('5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC', 1000 * 10 ** 12),
+    api.tx.balances.transferKeepAlive('5GDCKqWHVg6NG9RQeKQjAQ7t9Pc94UZ3FsxRDCNyesUudi8Y', 100 * 10 ** 12),
+  ]);
 
-      if (transferEvent) {
-        const [from, to, amount] = transferEvent.event.data;
-        console.log(`${from} transfered ${amount} to ${to}`);
-      } else {
-        console.log('No transfer event found');
+  await new Promise((resolve, reject) =>
+    tx.signAndSend(alice, ({ events, status }) => {
+      if (status.isInBlock) {
+        const transferEvent = events.find(
+          ({ event: { method, section } }) => section === 'system' && method === 'Transfer',
+        );
+
+        if (transferEvent) {
+          const [from, to, amount] = transferEvent.event.data;
+          console.log(`${from} transfered ${amount} to ${to}`);
+        } else {
+          console.log('No transfer event found');
+        }
+      } else if (status.isFinalized) {
+        resolve('Finalized');
       }
-    });
-
-  await api.tx.balances
-    .transferKeepAlive('5GDCKqWHVg6NG9RQeKQjAQ7t9Pc94UZ3FsxRDCNyesUudi8Y', 100 * 10 ** 12)
-    .signAndSend(bob, ({ events }) => {
-      const transferEvent = events.find(
-        ({ event: { method, section } }) => section === 'system' && method === 'Transfer',
-      );
-
-      if (transferEvent) {
-        const [from, to, amount] = transferEvent.event.data;
-        console.log(`${from} transfered ${amount} to ${to}`);
-      } else {
-        console.log('No transfer event found');
-      }
-    });
+    }),
+  );
 };
 
 main()
