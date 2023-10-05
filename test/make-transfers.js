e9c52e8ab29373f3676d088f0c3f6844245cb518
Dmitry Osipov <dmith.osipov@gmail.com>
2023-10-05 14:38:05 +0400

    fix: return set-prefunded accounts workflow

--- test/make-transfers.js
+++ test/make-transfers.js
@@ -9,10 +9,33 @@ const main = async () => {
 
   await api.tx.balances
     .transferKeepAlive('5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC', 1000 * 10 ** 12)
-    .signAndSend(alice);
+    .signAndSend(alice, ({ events }) => {
+      const transferEvent = events.find(
+        ({ event: { method, section } }) => section === 'system' && method === 'Transfer',
+      );
+
+      if (transferEvent) {
+        const [from, to, amount] = transferEvent.event.data;
+        console.log(`${from} transfered ${amount} to ${to}`);
+      } else {
+        console.log('No transfer event found');
+      }
+    });
+
   await api.tx.balances
     .transferKeepAlive('5GDCKqWHVg6NG9RQeKQjAQ7t9Pc94UZ3FsxRDCNyesUudi8Y', 100 * 10 ** 12)
-    .signAndSend(bob);
+    .signAndSend(bob, ({ events }) => {
+      const transferEvent = events.find(
+        ({ event: { method, section } }) => section === 'system' && method === 'Transfer',
+      );
+
+      if (transferEvent) {
+        const [from, to, amount] = transferEvent.event.data;
+        console.log(`${from} transfered ${amount} to ${to}`);
+      } else {
+        console.log('No transfer event found');
+      }
+    });
 };
 
 main()
--- test/set-prefunded-acc.js
+++ test/set-prefunded-acc.js
@@ -7,19 +7,23 @@ assert.notStrictEqual(configPath, undefined, 'Path to rosetta config file is not
 
 let spec = fs.readFileSync(path, 'utf-8');
 let config = fs.readFileSync(configPath, 'utf-8');
-const accounts = JSON.parse(config).construction.prefunded_accounts.map(({ account_identifier: { address }}) => 
-`          [
+const accounts = JSON.parse(config)
+  .construction.prefunded_accounts.map(
+    ({ account_identifier: { address } }) =>
+      `          [
             "${address}", 
-            1152921504606846976
+            10000000000000000
           ]
-`).join(',\n');
+`,
+  )
+  .join(',\n');
 
 const balance = `,
 ${accounts}
         ]
       }`;
 
-const match = spec.match(/"balances": \[[\w\W]+?\}/g)[0]
-json = spec.replace(match, match.replace(/\s*\]\s*\}/g, balance))
+const match = spec.match(/"balances": \[[\w\W]+?\}/g)[0];
+json = spec.replace(match, match.replace(/\s*\]\s*\}/g, balance));
 
-fs.writeFileSync(path, json)
+fs.writeFileSync(path, json);
