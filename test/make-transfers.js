5a9a60f3092f541d89613a6339630b622ca1e5f9
Dmitry Osipov <dmith.osipov@gmail.com>
2023-10-05 11:48:33 +0400

    update

--- test/make-transfers.js
+++ test/make-transfers.js
@@ -1,21 +1,23 @@
 const { ApiPromise } = require('@polkadot/api');
 const { Keyring } = require('@polkadot/keyring');
 
-
 const main = async () => {
   const api = await ApiPromise.create();
   const keyring = new Keyring();
   const alice = keyring.addFromUri('//Alice', {}, 'sr25519');
   const bob = keyring.addFromUri('//Bob', {}, 'sr25519');
 
-  await api.tx.balances.transferKeepAlive('5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC', 100000).signAndSend(alice)
-  await api.tx.balances.transferKeepAlive('5GDCKqWHVg6NG9RQeKQjAQ7t9Pc94UZ3FsxRDCNyesUudi8Y', 100000).signAndSend(bob)
-}
+  await api.tx.balances
+    .transferKeepAlive('5HChDnRNd2xn7rGsbJNqJrV5NmpALVKTY9YC2Zzeyu5rjzqC', 100 * 10 ** 12)
+    .signAndSend(alice);
+  await api.tx.balances
+    .transferKeepAlive('5GDCKqWHVg6NG9RQeKQjAQ7t9Pc94UZ3FsxRDCNyesUudi8Y', 100 * 10 ** 12)
+    .signAndSend(bob);
+};
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.log(error); 
+    console.log(error);
     process.exit(1);
   });
-
