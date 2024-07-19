be72ce6bc5ac97a5167a25a208a7408a363f7e5b
sergey filyanin <filianin.sergey@gmail.com>
2024-07-19 11:36:38 +0200

    Update renew-config.sh

--- scripts/renew-config.sh
+++ scripts/renew-config.sh
@@ -7,35 +7,20 @@ version=$2
 testnet_meta_url="https://github.com/gear-tech/gear/releases/download/v"$tag"/testnet_vara_runtime_v"$version"_metadata.scale"
 production_meta_url="https://github.com/gear-tech/gear/releases/download/v"$tag"/production_vara_runtime_v"$version"_metadata.scale"
 
-
 echo "Downloading testnet metadata from $testnet_meta_url"
-testnet_metadata=`curl -L  $testnet_meta_url`
+testnet_metadata=$(curl -L $testnet_meta_url)
 
 echo "Downloading production metadata from $production_meta_url"
-production_metadata=`curl -L  $production_meta_url`
+production_metadata=$(curl -L $production_meta_url)
 
 echo "Updating testnet config"
-#jq --arg testnet_metadata "$testnet_metadata" --argjson version "$version" \
-#    '.metadataRpc = $testnet_metadata | .specVersion = $version' \
-#    "$TESTNET_CONFIG_PATH" > tmp.$$.json \
-#    && mv tmp.$$.json "$TESTNET_CONFIG_PATH"
-
-jq --slurpfile testnet_metadata /tmp/testnet_metadata.json --argjson version "$(cat /tmp/version.json)" \
-    '.metadataRpc = $testnet_metadata | .specVersion = $version' \
+echo "$testnet_metadata" | jq --argjson version "$version" \
+    '.metadataRpc = input | .specVersion = $version' \
     "$TESTNET_CONFIG_PATH" > tmp.$$.json \
     && mv tmp.$$.json "$TESTNET_CONFIG_PATH"
 
-rm /tmp/testnet_metadata.json /tmp/version.json
-
 echo "Updating mainnet config"
-#jq --arg production_metadata "$production_metadata" --argjson version "$version" \
-#    '.metadataRpc = $production_metadata | .specVersion = $version' \
-#    "$MAINNET_CONFIG_PATH" > tmp.$$.json \
-#    && mv tmp.$$.json "$MAINNET_CONFIG_PATH"
-
-jq --slurpfile production_metadata /tmp/production_metadata.json --argjson version "$(cat /tmp/version.json)" \
-    '.metadataRpc = $production_metadata | .specVersion = $version' \
+echo "$production_metadata" | jq --argjson version "$version" \
+    '.metadataRpc = input | .specVersion = $version' \
     "$MAINNET_CONFIG_PATH" > tmp.$$.json \
     && mv tmp.$$.json "$MAINNET_CONFIG_PATH"
-
-rm /tmp/production_metadata.json /tmp/version.json
