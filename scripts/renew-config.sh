1b68cfe0d08bdc44d8a0ec3e4aec478dc135152c
sergey filyanin <filianin.sergey@gmail.com>
2024-07-19 11:27:14 +0200

    Update renew-config.sh

--- scripts/renew-config.sh
+++ scripts/renew-config.sh
@@ -15,13 +15,27 @@ echo "Downloading production metadata from $production_meta_url"
 production_metadata=`curl -L  $production_meta_url`
 
 echo "Updating testnet config"
-jq --arg testnet_metadata "$testnet_metadata" --argjson version "$version" \
+#jq --arg testnet_metadata "$testnet_metadata" --argjson version "$version" \
+#    '.metadataRpc = $testnet_metadata | .specVersion = $version' \
+#    "$TESTNET_CONFIG_PATH" > tmp.$$.json \
+#    && mv tmp.$$.json "$TESTNET_CONFIG_PATH"
+
+jq --slurpfile testnet_metadata /tmp/testnet_metadata.json --argjson version "$(cat /tmp/version.json)" \
     '.metadataRpc = $testnet_metadata | .specVersion = $version' \
     "$TESTNET_CONFIG_PATH" > tmp.$$.json \
     && mv tmp.$$.json "$TESTNET_CONFIG_PATH"
 
+rm /tmp/testnet_metadata.json /tmp/version.json
+
 echo "Updating mainnet config"
-jq --arg production_metadata "$production_metadata" --argjson version "$version" \
+#jq --arg production_metadata "$production_metadata" --argjson version "$version" \
+#    '.metadataRpc = $production_metadata | .specVersion = $version' \
+#    "$MAINNET_CONFIG_PATH" > tmp.$$.json \
+#    && mv tmp.$$.json "$MAINNET_CONFIG_PATH"
+
+jq --slurpfile production_metadata /tmp/production_metadata.json --argjson version "$(cat /tmp/version.json)" \
     '.metadataRpc = $production_metadata | .specVersion = $version' \
     "$MAINNET_CONFIG_PATH" > tmp.$$.json \
     && mv tmp.$$.json "$MAINNET_CONFIG_PATH"
+
+rm /tmp/production_metadata.json /tmp/version.json
