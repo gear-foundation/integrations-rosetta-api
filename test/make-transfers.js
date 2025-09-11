2a48a94f0d12f24e5ace6b0dd29d5219fc0f27e9
osipov-mit <dmith.osipov@gmail.com>
2025-09-11 10:58:16 +0000

    fix: test on CI

--- test/make-transfers.js
+++ test/make-transfers.js
@@ -1,22 +1,29 @@
-const { ApiPromise } = require('@polkadot/api');
-const { Keyring } = require('@polkadot/keyring');
+const { ApiPromise } = require("@polkadot/api");
+const { Keyring } = require("@polkadot/keyring");
 
 const main = async () => {
-  const api = await ApiPromise.create();
+  const api = await ApiPromise.create({ noInitWarn: true });
   const keyring = new Keyring();
-  const alice = keyring.addFromUri('//Alice', {}, 'sr25519');
-  const bob = keyring.addFromUri('//Bob', {}, 'sr25519');
+  const alice = keyring.addFromUri("//Alice", {}, "sr25519");
+  const bob = keyring.addFromUri("//Bob", {}, "sr25519");
 
   const tx = api.tx.utility.batchAll([
-    api.tx.balances.transferKeepAlive('kGkfymFqB4aBVunXCW69EzQRDtkZkUQB44GB4XJnwNwqmWyRN', 1000 * 10 ** 12),
-    api.tx.balances.transferKeepAlive('kGjgUsJv5wDK645fjZ7B8qx42fNMjCPQdmzbHhwBG3vEpQhij', 100 * 10 ** 12),
+    api.tx.balances.transferKeepAlive(
+      "kGkfymFqB4aBVunXCW69EzQRDtkZkUQB44GB4XJnwNwqmWyRN",
+      1000 * 1e12,
+    ),
+    api.tx.balances.transferKeepAlive(
+      "kGjgUsJv5wDK645fjZ7B8qx42fNMjCPQdmzbHhwBG3vEpQhij",
+      1000 * 1e12,
+    ),
   ]);
 
   await new Promise((resolve, reject) =>
     tx.signAndSend(alice, ({ events, status }) => {
       if (status.isInBlock) {
         const transferEvents = events.filter(
-          ({ event: { method, section } }) => section === 'balances' && method === 'Transfer',
+          ({ event: { method, section } }) =>
+            section === "balances" && method === "Transfer",
         );
 
         if (transferEvents.length > 0) {
@@ -25,10 +32,10 @@ const main = async () => {
             console.log(`${from} transfered ${amount} to ${to}`);
           });
         } else {
-          console.log('No transfer event found');
+          console.log("No transfer event found");
         }
       } else if (status.isFinalized) {
-        resolve('Finalized');
+        resolve("Finalized");
       }
     }),
   );
